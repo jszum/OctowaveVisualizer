cdbb973b2682dabd28c83777a9550a05a2c8a51a
jszum <jaroslaw.szumega@gmail.com>
2015-07-07 14:27:21 +0200

    Playing selected *.wav file with gstreamer

--- octo.py
+++ octo.py
@@ -4,33 +4,28 @@ import pygst
 pygst.require('0.10')
 import gst
 import gtk
+import gobject
 import sys
+import os
 
 name = ''
 
 class Main:
 
     def __init__(self):
-
-        self.pipeline = gst.Pipeline("pipeline")
-
-        self.audiotestsrc = gst.element_factory_make("audiotestsrc", "audio")
-        self.pipeline.add(self.audiotestsrc)
-
-        self.sink = gst.element_factory_make("alsasink", "sink")
-        self.pipeline.add(self.sink)
-
-        self.audiotestsrc.link(self.sink)
-        self.pipeline.set_state(gst.STATE_PLAYING)
-
+        self.pl = gst.element_factory_make("playbin2", "player")
+        self.pl.set_property('uri', 'file://'+os.path.abspath(name))
+        self.pl.set_state(gst.STATE_PLAYING)
 
 
 
 if __name__ == '__main__':
-    print( ' '.join(sys.argv) )
+    print(' '.join(sys.argv))
     name = sys.argv[1]
+    mainloop = gobject.MainLoop()
     start = Main()
-    gtk.main()
+    mainloop.run()
+    mainloop.quit()
 
 
 
