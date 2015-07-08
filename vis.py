155ad5c0f6836267dd695b75e88ae57706aa86e0
jszum <jaroslaw.szumega@gmail.com>
2015-07-08 12:08:06 +0200

    Putting visualiser into a class

--- vis.py
+++ vis.py
@@ -4,29 +4,36 @@ from scipy.io.wavfile import read
 import matplotlib.pyplot as plt
 import sys
 
-file = sys.argv[1]
+class Vis:
 
-numberOfPlots = 2
-figurePerPlot = 4
-f = 0
+    def __init__(self, filename=None):
+        self.numberOfPlots = 2
+        self.figurePerPlot = 4
+        self.filename = filename
 
-for plot in range(numberOfPlots):
-    plt.figure(plot)
-    plt.waitforbuttonpress()
+    def show(self):
+        for plot in range(self.numberOfPlots):
+            plt.figure(plot)
+            plt.waitforbuttonpress()
 
-    for fig in range(figurePerPlot):
-        input_data = read(file)
-        audio = input_data[1]
+            for fig in range(self.figurePerPlot):
+                input_data = read(self.filename)
+                audio = input_data[1]
 
-        plt.subplot(2, 2, fig + 1)
-        plt.plot(audio[0:-1])
-        plt.ylabel("Amplitude")
-        plt.xlabel("Time")
-        plt.title("Channel " + str(plot * figurePerPlot + fig))
+                plt.subplot(2, 2, fig + 1)
+                plt.plot(audio[0:-1])
+                plt.ylabel("Amplitude")
+                plt.xlabel("Time")
+                plt.title("Channel " + str(plot * self.figurePerPlot + fig))
 
-    mng = plt.get_current_fig_manager()
-    mng.resize(*mng.window.maxsize())
-    plt.show()
+            mng = plt.get_current_fig_manager()
+            mng.resize(*mng.window.maxsize())
+            plt.show()
 
+if __name__ == '__main__':
+    print(' '.join(sys.argv))
+    name = sys.argv[1]
+    visualiser = Vis(name)
+    visualiser.show()
 
 
