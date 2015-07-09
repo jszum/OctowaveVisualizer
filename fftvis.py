7384a51c8de4558d00d654bf12a4e7b45e1c6ed0
jszum <jaroslaw.szumega@gmail.com>
2015-07-09 20:22:32 +0200

    Trying to plot FFT with autocorelation

--- fftvis.py
+++ fftvis.py
@@ -6,7 +6,11 @@ import scipy
 import numpy
 import matplotlib.pyplot as plt
 import sys
+import vis
 
+def autocorr(x):
+    result = numpy.correlate(x, x, mode='full')
+    return result
 
 if __name__ == '__main__':
 
@@ -23,21 +27,40 @@ if __name__ == '__main__':
 
     normalized = [(element/2**8.)*2-1 for element in audio]
 
-    fft_signal = fft(normalized)
 
+    fft_signal = fft(normalized)
     fft_freq = fftfreq(len(fft_signal), 1.0/wavfreq)
 
+    n = autocorr(normalized)
+    afft_signal = fft(n)
+    afft_freq = fftfreq(len(afft_signal), 1.0/wavfreq)
+
+    # plt.plot(n)
+    # plt.show()
+
     plt.figure(1)
-    plt.subplot(2, 1, 1)
+    plt.subplot(4, 1, 1)
     plt.plot(fft_freq, numpy.abs(fft_signal))
     axes = plt.gca()
-    axes.set_xlim([0, 10000])
+    axes.set_xlim([0, 100000])
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(4, 1, 2)
     plt.plot(fft_freq, 10*scipy.log10(fft_signal))
     axes = plt.gca()
     axes.set_xscale('log')
-    axes.set_xlim([0, 10000])
+    axes.set_xlim([0, 100000])
+
+    plt.subplot(4, 1, 3)
+    plt.plot(afft_freq, abs(afft_signal)**2)
+    axes = plt.gca()
+    axes.set_xscale('linear')
+    axes.set_xlim([0, 100000])
+
+    plt.subplot(4, 1, 4)
+    plt.plot(afft_freq, 10*scipy.log10(afft_signal)**2)
+    axes = plt.gca()
+    axes.set_xscale('log')
+    axes.set_xlim([0, 100000])
 
     plt.show()
 
