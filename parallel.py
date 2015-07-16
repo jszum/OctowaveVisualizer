89cae7797ce85205b56462a1449b51d44b41c8d5
jszum <jaroslaw.szumega@gmail.com>
2015-07-16 16:35:30 +0200

    Parameter for switching between waveform and fft plotting.

--- parallel.py
+++ parallel.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
+
 import subprocess
 import sys
 
 name = sys.argv[1]
+option = int(sys.argv[2])
 
-subprocess.Popen(["./fftplayer.py", name])
+vis = ''
+
+if option == 0:
+    vis = "./waveform.py"
+else:
+    vis = "./fftplayer.py"
+
+subprocess.Popen([vis, name])
 subprocess.call(["./player.py", name])
